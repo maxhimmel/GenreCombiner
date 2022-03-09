56cb18606d0a3599ccdb1b66385869ea012c8595
Max <himmelmax@gmail.com>
2022-03-09 10:27:09 -0800

    added observable!

---
+++ app/utility/observable.ts
@@ -0,0 +1,31 @@
+import { EventHandler } from "./events/eventHandler";
+import { IEvent } from "./events/ievent";
+
+export class Observable<TItem>
+{
+    get item(): TItem {
+        return this._item;
+    }
+
+    set item( newItem:TItem ) {
+        if ( newItem !== this._item )
+        {
+            this._item = newItem;
+            this._changed.Invoke( this._owner, newItem );
+        }
+    }
+    
+    get changed(): IEvent<TItem> {
+        return this._changed;
+    }
+
+    private readonly _owner: any;
+    private _item: TItem;
+    private _changed: EventHandler<TItem> = new EventHandler();
+
+    constructor( owner: any, item: TItem )
+    {
+        this._owner = owner;
+        this._item = item;
+    }
+}
