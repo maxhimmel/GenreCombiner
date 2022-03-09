0f9ecd062d210f56937a37cff7e0cf0a4477996a
Max <himmelmax@gmail.com>
2022-03-08 19:34:52 -0800

    added random range utility

---
+++ app/utility/random.ts
@@ -0,0 +1,10 @@
+namespace Utility
+{
+    export class Random
+    {
+        static Range( min: number, max: number ): number
+        {
+            return Math.floor( Math.random() * ( max - min ) ) + min;
+        }
+    }
+}
