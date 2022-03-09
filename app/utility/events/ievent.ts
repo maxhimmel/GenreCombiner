30da3eb62b73d05f9a626a34b9ad71c7c7606706
Max <himmelmax@gmail.com>
2022-03-08 19:35:42 -0800

    added c# style event wrapper

---
+++ app/utility/events/ievent.ts
@@ -0,0 +1,10 @@
+namespace Events
+{
+    export type Signature<TArg extends EventArgs> = ( sender: any, args: TArg ) => void;
+
+    export interface IEvent<TArg extends EventArgs>
+    {
+        Subscribe( func: Signature<TArg> ): void;
+        Unsubscribe( func: Signature<TArg> ): void;
+    }
+}
