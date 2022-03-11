eb423b11df39aa0d46a3a90391b540b32a2b22dd
Max <himmelmax@gmail.com>
2022-03-11 07:49:32 -0800

    observables now report the change in value

--- app/utility/observable.ts
+++ app/utility/observable.ts
@@ -1,27 +1,44 @@
 import { EventHandler } from "./events/eventHandler";
 import { IEvent } from "./events/ievent";
 
+export class DeltaArgs<TItem>
+{
+    readonly current: TItem;
+    readonly prev: TItem;
+
+    constructor( current: TItem, prev: TItem )
+    {
+        this.current = current;
+        this.prev = prev;
+    }
+}
+
 export class Observable<TItem>
 {
-    get item(): TItem {
+    get item(): TItem
+    {
         return this._item;
     }
 
-    set item( newItem:TItem ) {
+    set item( newItem: TItem )
+    {
         if ( newItem !== this._item )
         {
+            const args = new DeltaArgs( newItem, this._item );
             this._item = newItem;
-            this._changed.invoke( this._owner, newItem );
+
+            this._changed.invoke( this._owner, args );
         }
     }
-    
-    get changed(): IEvent<TItem> {
+
+    get changed(): IEvent<DeltaArgs<TItem>>
+    {
         return this._changed;
     }
 
     private readonly _owner: any;
+    private readonly _changed: EventHandler<DeltaArgs<TItem>> = new EventHandler();
     private _item: TItem;
-    private _changed: EventHandler<TItem> = new EventHandler();
 
     constructor( owner: any, item: TItem )
     {
