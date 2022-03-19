7299ca4beadc46f8f7a0aebc18a13312af4b8d98
Max <himmelmax@gmail.com>
2022-03-18 17:01:39 -0700

    added awaiting helper based on a conditional

--- app/utility/async.ts
+++ app/utility/async.ts
@@ -2,6 +2,14 @@ export class AsyncUtil
 {
     private constructor() { }
 
+    static async until( conditional: () => boolean ): Promise<void>
+    {
+        while ( !conditional() )
+        {
+            await this.delay( 0 );
+        }
+    }
+
     static async delay( seconds: number ): Promise<void>
     {
         return new Promise( resolve =>
