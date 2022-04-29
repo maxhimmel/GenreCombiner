3f89961bd62f485b831da04518a5887547e5d222
Max <himmelmax@gmail.com>
2022-04-29 07:53:30 -0700

    adding html element extensions

--- app/utility/extensions.ts
+++ app/utility/extensions.ts
@@ -1,8 +1,13 @@
+import { AsyncUtil } from "./async";
+
 declare global
 {
     interface HTMLElement
     {
         reflow(): void;
+        removeAllChildren(): void;
+        waitForTransitionEnd(): Promise<void>
+        waitForAnimationEnd(): Promise<void>
     }
 }
 
@@ -10,6 +15,42 @@ declare global
 HTMLElement.prototype.reflow = function(): void
 {
     this.offsetHeight;
+}
+
+HTMLElement.prototype.removeAllChildren = function(): void
+{
+    while ( this.firstChild !== null )
+    {
+        this.lastChild?.remove();
+    }
+}
+
+HTMLElement.prototype.waitForTransitionEnd = async function (): Promise<void>
+{
+    let isTransitionComplete: boolean = false;
+    
+    const onTransitionEnd = (): void =>
+    {
+        isTransitionComplete = true;
+        this.removeEventListener( "transitionend", onTransitionEnd );
+    };
+    this.addEventListener( "transitionend", onTransitionEnd );
+
+    await AsyncUtil.until( () => isTransitionComplete );
+}
+
+HTMLElement.prototype.waitForAnimationEnd = async function (): Promise<void>
+{
+    let isAnimationComplete: boolean = false;
+
+    const onAnimationEnd = (): void =>
+    {
+        isAnimationComplete = true;
+        this.removeEventListener( "animationend", onAnimationEnd );
+    };
+    this.addEventListener( "animationend", onAnimationEnd );
+
+    await AsyncUtil.until( () => isAnimationComplete );
 }
 
 export { }
